21f5a5986f07e5dbf7a70b36b09b2058e5b51945
Brandon Yee <139765638+brandonyee-cs@users.noreply.github.com>
2026-02-27 01:35:03 -0500

    v2.0.3

--- scripts/deployment/j1j2_dmrg_rdm.jl
+++ scripts/deployment/j1j2_dmrg_rdm.jl
@@ -17,8 +17,6 @@ using LinearAlgebra
 # Configure threading properly for ITensors block sparse
 BLAS.set_num_threads(1)
 ITensors.enable_threaded_blocksparse()
-using NDTensors
-NDTensors.Strided.disable_threads()
 
 function site_to_index(ix::Int, iy::Int, L::Int)::Int
     return (iy - 1) * L + ix
